96372db6c0c36ab4cb221718e55234f97b8f3308
Minwooooooo <110237141+Minwooooooo@users.noreply.github.com>
2022-09-17 17:51:45 +0900

    Add files via upload

---
+++ src/main/java/com/test/WebSocketPrac/WebSocketPracApplication.java
@@ -0,0 +1,13 @@
+package com.test.WebSocketPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketPracApplication.class, args);
+	}
+
+}
